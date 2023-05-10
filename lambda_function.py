93b6af6436fd5cf85b134fd6d62586018212f881
manolla gautham <manolla.gautham@gmail.com>
2023-05-10 21:10:05 +0300

    Add files via upload

---
+++ lambda_function.py
@@ -0,0 +1,22 @@
+import os
+from github import Github
+
+def lambda_handler(event, context):
+    """Lambda function wrapper
+    Args:
+        event: trigger event dict
+        context: lambda methods and properties
+    Returns:
+        string: greeting response
+    """
+    print('Starting functions\n---------------------------------------------')
+
+    # Retrieve the token from the environment variable
+    token = os.environ.get('GITHUB_TOKEN')
+
+    # Instantiate the Github object using the token
+    g = Github(token)
+
+    # Use the Github object to interact with the GitHub API
+    # ...
+
