586af9d9937b6d0d1539c25d4f9683587c8949dc
manolla gautham <manolla.gautham@gmail.com>
2023-05-10 23:46:36 +0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -10,12 +10,4 @@ def lambda_handler(event, context):
     """
     print('Starting functions\n---------------------------------------------')
 
-    # Retrieve the token from the environment variable
-    #token = os.environ.get( 'GITHUB_TOKEN')
-
-    # Instantiate the Github object using the token
-    #g = Github(token)
-
-    # Use the Github object to interact with the GitHub API
-    # ...
 
